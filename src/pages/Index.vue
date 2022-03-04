c194b45ee2ec80ea41ff63ef3ce72034f25fc23a
luannutels <luannutels@gmail.com>
2022-03-04 19:54:13 -0300

    config axios

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,9 +3,24 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
-
-export default defineComponent({
+import api from "../services/api";
+export default {
   name: "PageIndex",
-});
+
+  beforeMount() {
+    api
+      .get("/pokemon/2/")
+      .then((response) => {
+        // handle success
+        console.log(response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      });
+  },
+};
 </script>
