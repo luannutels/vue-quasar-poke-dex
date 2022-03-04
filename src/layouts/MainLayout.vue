fbd74ffbbce6b01caef8ba795b3939612162eb81
luannutels <luannutels@gmail.com>
2022-03-04 19:20:37 -0300

    title text-center

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title> Pokedex </q-toolbar-title>
+        <q-toolbar-title class="text-center"> Pokedex </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
